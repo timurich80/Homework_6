5ea9fc51fca9ba3ae5f3b74b11a18201d8696dc6
timurich80 <timurich80@gmail.com>
2022-09-15 11:39:52 +0300

    код работает

--- homework2/Program.cs
+++ homework2/Program.cs
@@ -4,4 +4,30 @@
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) 
 */
 
+double[] inputNums()
+{
+    Console.WriteLine("Введите значения через пробел: \n");
 
+    string [] stringNums = Console.ReadLine ().Split(' ');
+    double[] nums = new double[stringNums.Length];
+    for(int i=0; i<stringNums.Length; i++)
+    {
+        nums[i]  = Convert.ToDouble(stringNums[i]);
+    }
+    return nums;
+}
+
+void printFn(double[ ] nums)
+
+{
+    double b1 = nums[0];
+    double k1 = nums[1];
+    double b2 = nums[2];
+    double k2 = nums[3];
+
+    double x = (b2 - b1) /(k1 - k2);
+    double y = k1*x + b1;
+    System.Console.WriteLine($"Точка пересечения ({x}, {y})");
+}
+
+printFn(inputNums());
